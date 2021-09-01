5aabb8168ffe52d6fac13d0ff7227ce4423348bc
Peter Segal <pesegal@gmail.com>
2021-09-01 15:58:46 -0700

    Add Generated Schema

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,6 +20,8 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("com.graphql-java:graphql-java:11.0")
+	implementation("com.graphql-java:graphql-java-spring-boot-starter-webmvc:1.0")
 	implementation("org.postgresql:postgresql")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
