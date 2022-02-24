b7cabd0e6c0157d8ce31dc3d36680056e011256a
Peter Segal <pesegal@gmail.com>
2022-02-23 16:10:17 -0800

    refactor graphQL schema and config

--- src/main/kotlin/mf/focus/backend/GraphQLConfig.kt
+++ src/main/kotlin/mf/focus/backend/GraphQLConfig.kt
@@ -1,42 +1,28 @@
 package mf.focus.backend
 
-import org.springframework.util.ResourceUtils
-
 import graphql.GraphQL
-import graphql.schema.Coercing
-import graphql.schema.CoercingParseLiteralException
-import graphql.schema.CoercingParseValueException
-import graphql.schema.CoercingSerializeException
-import graphql.schema.GraphQLScalarType
-import graphql.schema.GraphQLSchema
-import graphql.schema.idl.RuntimeWiring
-import graphql.schema.idl.SchemaGenerator
-import graphql.schema.idl.SchemaParser
-import graphql.schema.idl.TypeDefinitionRegistry
-import graphql.schema.idl.TypeRuntimeWiring
+import graphql.schema.*
+import graphql.schema.idl.*
 import mf.focus.backend.fetchers.HelloFetcher
 import mf.focus.backend.fetchers.ProjectDataFetchers
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.util.ResourceUtils
 import java.io.File
 import java.time.LocalDateTime
 import java.time.format.DateTimeParseException
 
 @Configuration
-class GraphQLConfig {
-
-    @Autowired
-    lateinit var projectDataFetchers: ProjectDataFetchers
-
-    @Autowired
-    lateinit var helloFetcher: HelloFetcher
+class GraphQLConfig(
+    val projectDataFetchers: ProjectDataFetchers,
+    val helloFetcher: HelloFetcher
+) {
 
     @Bean
     fun graphQL(): GraphQL {
         val schema = ResourceUtils.getFile("classpath:schema.graphqls")
         val graphQlSchema : GraphQLSchema = buildSchema(schema)
-        return GraphQL.newGraphQL(graphQlSchema).build();
+        return GraphQL.newGraphQL(graphQlSchema).build()
     }
 
     fun buildSchema(schema: File): GraphQLSchema {
@@ -49,7 +35,7 @@ class GraphQLConfig {
         return RuntimeWiring.newRuntimeWiring()
             .scalar(dateTimeScalar())
             .type(TypeRuntimeWiring.newTypeWiring("Query").dataFetcher("hello", helloFetcher.getHello()))
-            //.type(TypeRuntimeWiring.newTypeWiring("Query").dataFetcher("getProjects", projectDataFetchers.getProjects()))
+            .type(TypeRuntimeWiring.newTypeWiring("Query").dataFetcher("getProjects", projectDataFetchers.getProjects()))
             .build()
     }
 
